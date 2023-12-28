e268491ddc3899a7e0791413ae3073a2ddd0bd36
Tanzeebul Tamim <tamim200091@gmail.com>
2023-12-28 14:37:41 +0600

    interface and model added

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,9 @@
-import cors from 'cors';
-import express, { Application, Request, Response } from 'express';
-
+import express, { Application } from 'express';
+const port = 3000;
 const app: Application = express();
 
-// parsers
-app.use(express.json());
-app.use(cors());
-
-app.get('/', (req: Request, res: Response) => {
-  res.send('Hello World!');
+app.listen(port, () => {
+  console.log(`Your app, Assignment-2 is listening on port ${port}`);
 });
 
 export default app;
--- src/server.ts
+++ src/server.ts
@@ -7,7 +7,7 @@ async function main() {
     await mongoose.connect(config.database_url as string);
 
     app.listen(config.port, () => {
-      console.log(`Assignment-2 app is listening on port ${config.port}`);
+      console.log(`Your app, Assignment-2 is listening on port ${config.port}`);
     });
   } catch (err) {
     console.log(err);
